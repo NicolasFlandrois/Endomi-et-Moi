5fcb60cb218903e9eefe646e18c65e3d238ac9b4
NicolasFlandrois <n.flandrois9github@gmail.com>
2020-06-25 12:44:42 +0200

    Admin Updates - Added a Pannel Controle for all constants
    Allow the Admin to manage Constants' table in database

--- trackers/admin.py
+++ trackers/admin.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 # Author: Nicolas Flandrois
 # Date:   Tue 16 June 2020 14:23:33
-# Last Modified time: Tue 16 June 2020 14:37:19 
+# Last Modified time: Thu 25 June 2020 12:07:31 
 
-# Description:
+# Description: Allowing to manage database's constants from admin console
 
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Constants)
--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author: Nicolas Flandrois
 # Date:   Tue 02 June 2020 15:28:15
-# Last Modified time: Tue 16 June 2020 14:40:44 
+# Last Modified time: Thu 25 June 2020 10:02:35 
 
 # Description:
 
