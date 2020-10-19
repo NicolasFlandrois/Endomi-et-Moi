1d7c8e8354e176916e988b9168bf0d4b9ab3bcae
NicolasFlandrois <n.flandrois9github@gmail.com>
2020-10-19 22:45:17 +0200

    Travis Setting SetupUpdate

--- endomi/travis_settings.py
+++ endomi/travis_settings.py
@@ -6,8 +6,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'endomi',
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASS'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASS'),
         'HOST': 'localhost',
         'PORT': '3306',
     },
